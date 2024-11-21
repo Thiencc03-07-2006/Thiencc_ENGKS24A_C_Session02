f94aba31f4b5697b123606021ce37f2a87d27d1f
Thiencc03-07-2006 <cct20062250.@gmail.com>
2024-11-21 13:51:29 +0700

    Upload bài tập session02-2

---
+++ session02-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int num1,num2;
+	printf ("nhap so 1 ");
+	scanf("%d",&num1);
+	printf ("nhap so 2 ");
+	scanf("%d",&num2);
+	int total=num1+num2;
+	printf("%d + %d = %d",num1,num2,total);
+	return 0;
+}
